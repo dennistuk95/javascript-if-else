5f72b737457a4273910153d04f56c0e4f561f323
dennistuk95 <88230815+dennistuk95@users.noreply.github.com>
2021-09-18 12:46:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 26;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age > 17) {
+    console.log("Je mag naar binnen omdat je 18+ bent");
+} else {
+    console.log("Je mag niet naar binnen omdat je onder de 18 bent");
+}
+
+if (isFemale) {
+    console.log("Je mag naar binnen op de ladiesnight");
+} else {
+    console.log("Je mag niet naar binnen op de ladiesnight");
+}
+
+if (driverStatus == 'bob') {
+    console.log("Je mag naar huis rijden");
+} else {
+    console.log("Je mag niet naar huis rijden");
+}
